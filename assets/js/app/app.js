3d760aad0a8e1f3e9403fadac636321643e3b267
Swedish-li <one.piece.li.2016@gmail.com>
2017-03-23 23:52:38 +0800

    添加Vue.js组件示例

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,9 +1,53 @@
-define(function (require) {
-	var $ = require('jquery')
-	var Vue = require('vue')
-	var data = require('data')
+define(function(require) {
+    var $ = require('jquery')
+    var Vue = require('vue')
+    var data = require('data')
 
-	var app = new Vue({
+    //组件之间的数据传递
+    Vue.component('child',{
+        props: ['message'],
+        template : '<span>{{message}}</span>'
+    })
+
+    new Vue({
+        el: '#example-5'
+    })
+
+    //simple counter
+    Vue.component('simple-counter',{
+        template: '<button @click="counter+=1">{{counter}}</button>',
+        data :function(){
+            return {counter: 0}
+        }
+    })
+
+    new Vue({
+        el: '#example-4'
+    })
+
+    //局部注册组件,这种方式也可以注册其他vue功能，如指令
+    var child = {
+        template: '<h1>This ia child component!</h1>'
+    }
+
+    new Vue({
+        el: '#example-3',
+        components :{
+            'new-component': child
+        }
+    })
+
+    //全局注册组件
+    Vue.component('my-component',{
+        template : '<h1>Hello Vue component!</h1>'
+    })
+
+    //创建Vue实例
+    new Vue({
+        el: '#example-2'
+    })
+
+    var app = new Vue({
         el: '#app',
         data: {
             tableData: data.tableData
@@ -21,22 +65,25 @@ define(function (require) {
     })
 
     var app2 = new Vue({
-        el:'#app2',
+        el: '#app2',
         data: {
-            tableData:data.tableData
+            tableData: data.tableData,
+            classItem: {}
         },
-        methods:{
-            isSelected:function(index){
-                $("#app2>table>tbody>tr").removeClass("selected")
-                $("#app2>table>tbody>tr:eq("+index+")").addClass("selected")
+        methods: {
+            isSelected: function(index) {
+                console.log(index)
+                    this.$set(this.classItem,index)
+                    //$($el).find("table>tbody>tr").removeClass("selected")
+                    //$($el).find("table>tbody>tr:eq("+index+")").addClass("selected")
             }
         }
     })
 
     var example1 = new Vue({
-        el:'#example-1',
-        data:{
-            count : 0
+        el: '#example-1',
+        data: {
+            count: 0
         }
     })
 })
