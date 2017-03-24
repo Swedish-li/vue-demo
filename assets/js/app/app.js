22b3d92f9c3d8862aee99ce729b916723a0cdafb
Swedish-li <one.piece.li.2016@gmail.com>
2017-03-24 22:42:56 +0800

    示例完善

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -3,10 +3,18 @@ define(function(require) {
     var Vue = require('vue')
     var data = require('data')
 
+    //父组件使用属性想子组件传递数据(props down)，子组件通过事件向父组件传递数据(event up)
     //组件之间的数据传递
-    Vue.component('child',{
+    Vue.component('child', {
         props: ['message'],
-        template : '<span>{{message}}</span>'
+        template: '<h3>{{message}}</h3>'
+    })
+
+    new Vue({
+        el: '#example-6',
+        data: {
+            msg: "this is parent component!"
+        }
     })
 
     new Vue({
@@ -14,10 +22,12 @@ define(function(require) {
     })
 
     //simple counter
-    Vue.component('simple-counter',{
+    Vue.component('simple-counter', {
         template: '<button @click="counter+=1">{{counter}}</button>',
-        data :function(){
-            return {counter: 0}
+        data: function() {
+            return {
+                counter: 0
+            }
         }
     })
 
@@ -31,15 +41,15 @@ define(function(require) {
     }
 
     new Vue({
-        el: '#example-3',
-        components :{
-            'new-component': child
-        }
-    })
-
-    //全局注册组件
-    Vue.component('my-component',{
-        template : '<h1>Hello Vue component!</h1>'
+            el: '#example-3',
+            components: {
+                'new-component': child
+            }
+        })
+        //Vue.component(tagName, options)
+        //全局注册组件
+    Vue.component('my-component', {
+        template: '<h1>Hello Vue component!</h1>'
     })
 
     //创建Vue实例
@@ -73,7 +83,7 @@ define(function(require) {
         methods: {
             isSelected: function(index) {
                 console.log(index)
-                    this.$set(this.classItem,index)
+                this.$set(this.classItem, index)
                     //$($el).find("table>tbody>tr").removeClass("selected")
                     //$($el).find("table>tbody>tr:eq("+index+")").addClass("selected")
             }
