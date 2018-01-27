b2321c47024bbe332daf29ae81856e8c5c4975fe
Swedish-li <one.piece.li.2016@gmail.com>
2018-01-27 19:25:19 +0800

    说明添加，样式修改

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # vue-demo
+
+查看示例
+
+```bash
+$ yarn start
+```
+
 参考 [requirejs-bootstrap-demo](https://github.com/sudo-cm/requirejs-bootstrap-demo)
+
--- assets/js/app/component/simple-counter.js
+++ assets/js/app/component/simple-counter.js
@@ -4,7 +4,7 @@ define([
     'use strict';
     //simple counter
     Vue.component('simple-counter', {
-        template: '<button @click="counter+=1">{{counter}}</button>',
+        template: '<button class="btn btn-danger" @click="counter+=1">{{counter}}</button>',
         data: function () {
             return {
                 counter: 0
